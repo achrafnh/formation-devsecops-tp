ccca216de3b4753b63d8c59128d72c15256f1738
herber <herberthknight2@gmail.com>
2024-02-06 11:33:11 +0100

    changes11-32

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,5 +8,26 @@ pipeline {
               archive 'target/*.jar' //so that they can be downloaded later test aa
             }
         }   
+
+    stage('UNIT test & jacoco ') {
+
+      steps {
+
+        sh "mvn test"
+
+      }
+
+      post {
+
+        always {
+
+          junit 'target/surefire-reports/*.xml'
+
+          jacoco execPattern: 'target/jacoco.exec'
+
+        }
+
+      }
     }
+  }
 }
--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -2,23 +2,17 @@ package com.devsecops;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
+	private static final String baseURL = "http://localhost:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -50,5 +44,4 @@ public class NumericController {
 			return Integer.parseInt(response);
 		}
 	}
-
 }
