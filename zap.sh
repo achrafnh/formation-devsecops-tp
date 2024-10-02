f38c9b68288ffbe897f19319378ccd2b396e1794
NHAILA Achraf <anhaila@leyton.com>
2024-10-02 16:16:50 +0200

    new stages

--- zap.sh
+++ zap.sh
@@ -28,7 +28,8 @@ echo "Exit Code : $exit_code"
 
  if [[ ${exit_code} -ne 0 ]];  then
     echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
-    docker run -d --rm --name zap-report-server -p 8888:80 -v $(pwd):/usr/share/nginx/html nginx
+    docker stop zap-report-server
+    docker run -d --rm --name zap-report-server -p 8888:80 -v $(pwd)/owasp-zap-report:/usr/share/nginx/html nginx
     exit 1;
    else
     echo "OWASP ZAP did not report any Risk"
